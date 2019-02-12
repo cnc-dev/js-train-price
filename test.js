2f05a7fb1f9ead16502e2484b6966d47381fbf6c
cnc-dev <acforex89@gmail.com>
2019-02-12 20:57:48 +0100

    start

---
+++ test.js
@@ -0,0 +1,7 @@
+var Età = prompt ("quanti anni hai?"){
+var Distanza = prompt ("benissimo, ora dimmi quanti km hai percorso")
+// var CostoDelBiglietto = alert ("premi Ok per scoprire il costo del biglietto")
+  if (Età < 18){
+   alert("Distanza * 0,168");
+  }
+}
