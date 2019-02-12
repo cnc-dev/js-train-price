00220936edae58402b370257463dad121ac3f60a
cnc-dev <acforex89@gmail.com>
2019-02-12 21:57:32 +0100

    Fine

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
-var Età = prompt ("quanti anni hai?"){
-var Distanza = prompt ("benissimo, ora dimmi quanti km hai percorso")
-// var CostoDelBiglietto = alert ("premi Ok per scoprire il costo del biglietto")
-  if (Età < 18){
-   alert("Distanza * 0,168");
-  }
+var Età = prompt ("quanti anni hai?");
+var Distanza = prompt ("benissimo, ora dimmi quanti km hai percorso");
+var Prezzo = 0.21;
+var ScontoMinorenni = (Prezzo*20) / 100;
+var ScontoAnziani = (Prezzo*40) / 100;
+
+
+if (Età <= 18){
+  Prezzo = Prezzo - ScontoMinorenni;
+  document.write ("Il costo del biglietto è di euro "+ Prezzo * Distanza );
+}
+
+if (Età >= 65){
+  Prezzo = Prezzo - ScontoAnziani;
+  document.write ("Il costo del biglietto è di euro "+ Prezzo * Distanza );
+}
+
+else if (Età >18){
+  Prezzo = Prezzo;
+  document.write ("Il costo del biglietto è di euro "+ Prezzo * Distanza );
 }
